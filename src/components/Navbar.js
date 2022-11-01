b85926ecbd0d081ee30ca343ff4cd8bbbe7cc1b8
Piyush <piyushbansal941@gmail.com>
2022-11-01 23:48:23 +0530

    url change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,17 +5,17 @@ function Navbar() {
       <div className="container-fluid px-0 sticky-top">
           <nav className="navbar navbar-expand-lg navbar-light bg-light">
               <div className="container-fluid">
-                  <a className="navbar-brand" href="/react-php-crud-app/#/">Garage Database</a>
+                  <a className="navbar-brand" href="/parking-app/#/">Garage Database</a>
                   <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                   <span className="navbar-toggler-icon"></span>
                   </button>
                   <div className="collapse navbar-collapse" id="navbarSupportedContent">
                       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                           <li className="nav-item">
-                              <a className="nav-link active" id="home_link" aria-current="page" href="/react-php-crud-app/#/">Home</a>
+                              <a className="nav-link active" id="home_link" aria-current="page" href="/parking-app/#/">Home</a>
                           </li>
                           <li className="nav-item">
-                              <a className="nav-link " id="list_link" href="/react-php-crud-app/#/list">Details</a>
+                              <a className="nav-link " id="list_link" href="/parking-app/#/list">Details</a>
                           </li>
                           
                       </ul>
